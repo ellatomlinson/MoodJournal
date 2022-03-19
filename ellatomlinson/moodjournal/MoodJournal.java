4c2497482453f57c79ced6013ec8c3c24ed20aef
ella.tomlinson <ella.tomlinson@ucalgary.ca>
2022-03-19 17:11:28 -0600

    Deleted unnecessary import

--- ellatomlinson/moodjournal/MoodJournal.java
+++ ellatomlinson/moodjournal/MoodJournal.java
@@ -2,7 +2,6 @@ package ellatomlinson.moodjournal;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class MoodJournal {
